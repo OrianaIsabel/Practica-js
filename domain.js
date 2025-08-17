ba207ea96cd06b4e6c012fb16b2d498f6b0e361d
OrianaIsabel <obellorinmata@frba.utn.edu.ar>
2025-08-16 22:26:52 -0300

    hola

--- domain.js
+++ domain.js
@@ -1,1 +1,112 @@
+const Categoria = Object.freeze({
+    Hotel: "Hotel",
+    Departamento: "Departamento",
+    Cabana: "Cabana",
+    Apart: "Apart",
+})
 
+class Alojamiento {
+    constructor(nombre, precioPorNoche, categoria) {
+        this.nombre = nombre
+        this.precioPorNoche = precioPorNoche
+        this.categoria = categoria
+        this.reservas = []
+    }
+
+    getDescripcion() {
+        return `${this.nombre} (${this.categoria}) ${this.precioPorNoche} por noche`
+    }
+
+    agregarReserva(reserva) {
+        this.reservas.push(reserva)
+    }
+}
+
+class Reserva {
+    constructor(alojamiento, diaInicio, diaFin) {
+        if (!(diaInicio instanceof Date) || !(diaFin instanceof Date)) {
+            throw new Error("El Dia de inicio y de fin deben ser una instancia de Date")
+        }
+
+        if (alojamiento.reservas.some(
+            (a) => {return (a.diaInicio <= diaInicio && a.diaFin >= diaInicio) || (a.diaInicio <= diaFin && a.diaFin >= diaFin)})
+            ) {
+            throw new Error("Se esta superponiendo con otra reserva en el mismo alojamiento")
+        }
+
+        this.alojamiento = alojamiento
+        this.diaInicio = diaInicio
+        this.diaFin = diaFin
+        this.descuentos = []
+
+        alojamiento.agregarReserva(this)
+    }
+
+    cantidadNoches() {
+        const msPorDia = 1000 * 60 * 60 * 24;
+        return Math.ceil((this.diaFin - this.diaInicio) / msPorDia); // Para redondear hacia arriba
+    }
+
+    precioBase() {
+        return this.cantidadNoches() * this.alojamiento.precioPorNoche;
+    }
+
+    precioFinal() {
+        let base = this.precioBase();
+        let totalDescontado = 0;
+        for (const desc of this.descuentos) {
+            totalDescontado += desc.valorDescontado(base, this.cantidadNoches());
+        }
+        return Math.max(0, base - totalDescontado);
+    }
+
+    agregarDescuento(descuento) {
+        this.descuentos.push(descuento);
+    }
+}
+
+class DescuentoFijo {
+  constructor(valor) {
+    this.valor = valor;
+  }
+
+  valorDescontado(precioBase, cantidad) {
+    return this.valor;
+  }
+}
+
+class DescuentoPorcentaje {
+  constructor(porcentaje) {
+    // Recibe por ejemplo, 10
+    this.porcentaje = porcentaje;
+  }
+
+  valorDescontado(precioBase, cantidad) {
+    return precioBase * (this.porcentaje / 100);
+  }
+}
+
+class DescuentoPorNoches {
+  constructor(cantidadMinima, porcentaje) {
+    this.cantidadMinima = cantidadMinima;
+    this.porcentaje = porcentaje;
+  }
+
+  valorDescontado(precioBase, cantidad) {
+    const vecesRepetido = Math.floor(cantidad / this.cantidadMinima); // Para redondear hacia abajo
+    let valorDescontado = 0;
+    if (vecesRepetido >= 1) {
+      valorDescontado = precioBase * (this.porcentaje / 100) * vecesRepetido;
+    }
+    return valorDescontado;
+  }
+}
+
+module.exports = {
+    Categoria,
+    Alojamiento,
+    Reserva,
+    DescuentoFijo,
+    DescuentoPorcentaje,
+    DescuentoPorNoches,
+};
---
+++ index.js
@@ -0,0 +1,46 @@
+const {
+  Alojamiento,
+  Categoria,
+  Reserva,
+  DescuentoFijo,
+  DescuentoPorcentaje,
+  DescuentoPorNoches,
+} = require("./domain.js");
+
+const {
+    aumentarPrecioDiario,
+    alojamientoMasCaro,
+    filtrarPorPrecio,
+    obtenerTotalReservas,
+} = require("./funciones.js")
+
+const alojamiento1 = new Alojamiento(
+  "Hotel en Buenos Aires",
+  100,
+  Categoria.Hotel
+)
+
+console.log(alojamiento1.getDescripcion())
+
+const reserva1 = new Reserva(
+  alojamiento1,
+  new Date("2025-05-10"),
+  new Date("2025-05-20")
+);
+
+reserva1.agregarDescuento(new DescuentoFijo(100));
+reserva1.agregarDescuento(new DescuentoFijo(50));
+reserva1.agregarDescuento(new DescuentoPorcentaje(10));
+reserva1.agregarDescuento(new DescuentoPorNoches(2, 5));
+
+const catalogo = [
+  new Alojamiento("Apart1", 10, Categoria.Apart),
+  new Alojamiento("Apart2", 2000, Categoria.Apart),
+  new Alojamiento("Apart3", 100, Categoria.Hotel),
+];
+
+const reservas = [
+  new Reserva(alojamiento1, new Date("2025-05-01"), new Date("2025-05-10")),
+  new Reserva(alojamiento1, new Date("2025-05-01"), new Date("2025-05-7")),
+  new Reserva(alojamiento1, new Date("2025-05-01"), new Date("2025-05-8")),
+];
